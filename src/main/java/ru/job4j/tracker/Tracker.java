31e56693c8147381dafbc054217eeffc6926ac07
dmitrykachala <kosas@mail.ru>
2021-09-03 06:59:34 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #198059]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -63,8 +63,7 @@ public class Tracker {
             item.setId(id);
             items[index] = item;
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
