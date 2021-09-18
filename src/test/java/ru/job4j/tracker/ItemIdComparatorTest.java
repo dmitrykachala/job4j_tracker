3c6ad5262a1744ee5dedec9ee5c5da2d0a1f1395
dmitryu.kachala <kosas@mail.ru>
2021-09-19 00:02:02 +0300

    0. Сортировка [#10096 #204581]

--- src/test/java/ru/job4j/tracker/ItemIdComparatorTest.java
+++ src/test/java/ru/job4j/tracker/ItemIdComparatorTest.java
@@ -12,21 +12,25 @@ public class ItemIdComparatorTest {
 
     @Test
     public void compare1() {
-        List<Item> items = Arrays.asList(new Item(3, "aaa"), new Item(1, "mmm"),
-                new Item(13, "ggg"), new Item(0, "ttt"));
-        List<Item> expected = Arrays.asList(new Item(13, "ggg"), new Item(3, "aaa"),
-                new Item(1, "mmm"), new Item(0, "ttt"));
+        Item item1 = new Item(3, "aaa");
+        Item item2 = new Item(1, "mmm");
+        Item item3 = new Item(13, "ggg");
+        Item item4 = new Item(0, "ttt");
+        List<Item> items = Arrays.asList(item1, item2, item3, item4);
+        List<Item> expected = Arrays.asList(item3, item1, item2, item4);
         Collections.sort(items, new ItemIdComparator());
-        assertEquals(expected.toString(), items.toString());
+        assertEquals(expected, items);
     }
 
     @Test
     public void compareMinus1() {
-        List<Item> items = Arrays.asList(new Item(3, "aaa"), new Item(1, "mmm"),
-                new Item(13, "ggg"), new Item(0, "ttt"));
-        List<Item> expected = Arrays.asList(new Item(0, "ttt"), new Item(1, "mmm"),
-                new Item(3, "aaa"), new Item(13, "ggg"));
+        Item item1 = new Item(3, "aaa");
+        Item item2 = new Item(1, "mmm");
+        Item item3 = new Item(13, "ggg");
+        Item item4 = new Item(0, "ttt");
+        List<Item> items = Arrays.asList(item1, item2, item3, item4);
+        List<Item> expected = Arrays.asList(item4, item2, item1, item3);
         Collections.sort(items, new ItemIdComparator().reversed());
-        assertEquals(expected.toString(), items.toString());
+        assertEquals(expected, items);
     }
 }
