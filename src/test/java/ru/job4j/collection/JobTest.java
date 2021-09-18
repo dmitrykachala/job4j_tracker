44724d988baed2421f7cc676ef76360798150756
dmitryu.kachala <kosas@mail.ru>
2021-09-18 14:55:43 +0300

    2. Комбинированный компаратор [#10036 #204724]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -14,9 +14,9 @@ public class JobTest {
                 .thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 1)
         );
-        assertThat(rsl, lessThan(0));
+        assertThat(rsl, greaterThan(0));
     }
 
     @Test
@@ -24,10 +24,10 @@ public class JobTest {
         Comparator<Job> cmpNamePriority = new JobAscByName()
                 .thenComparing(new JobAscByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
+                new Job("Fix bug", 0),
                 new Job("Fix bug", 1)
         );
-        assertThat(rsl, greaterThan(0));
+        assertThat(rsl, lessThan(0));
     }
 
     @Test
