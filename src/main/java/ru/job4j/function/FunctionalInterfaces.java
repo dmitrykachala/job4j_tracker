01140805318a0cd7efcee2ec8ad0c84eda28008c
dmitryu.kachala <kosas@mail.ru>
2021-09-20 21:21:45 +0300

    1.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -11,35 +11,25 @@ public class FunctionalInterfaces {
         Map<Integer, String> map = new HashMap<>();
         List<String> list = List.of("one", "two", "three", "four", "five", "six", "seven");
         BiConsumer<Integer, String> biCon = (i, s) -> map.put(i, s);
-        int i = 1;
+        int j = 1;
         for (String l : list) {
-            biCon.accept(i++, l);
+            biCon.accept(j++, l);
         }
 
-        BiPredicate<Integer, String> biPred = (integer, s) -> map.get(integer).;
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
-            /*
-                Замените проверку в if() на использование BiPredicate, он объявлен выше, требуется его реализовать.
-             */
-            if (i % 2 == 0 || map.get(i).length() == 4) {
+            if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
         }
-        /*
-            Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
-         */
-        Supplier<List<String>> sup = ...;
-        List<String> strings = new ArrayList<>(map.values());
 
-        Consumer<String> con = ...;
-        Function<String, String> func = ...;
+        Supplier<List<String>> sup = () -> (List<String>) map.values();
+        List<String> strings = sup.get();
+
+        Consumer<String> con = string -> System.out.println(string);
+        Function<String, String> func = string -> string.toUpperCase();
         for (String s : strings) {
-            /*
-                Заменить вывод строк на применение Consumer
-                Заменить преобразование строк к строкам в верхнем регистре с помощью Function
-                Необходимое объявлено выше, требуется их реализовать.
-            */
-            System.out.println(s.toUpperCase());
+            con.accept(func.apply(s));
         }
     }
 }
