4a3a16ca623af2a85305b324a22ee7f0435f6d47
dmitryu.kachala <kosas@mail.ru>
2021-09-21 21:40:42 +0300

    6. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.*;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList();
@@ -10,12 +11,15 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> name = p -> p.getName().contains(key);
+        Predicate<Person> sname = p -> p.getSurname().contains(key);
+        Predicate<Person> address = p -> p.getAddress().contains(key);
+        Predicate<Person> phone = p -> p.getPhone().contains(key);
+        Predicate<Person> combine = p -> name.test(p) || sname.test(p)
+                                    || address.test(p) || phone.test(p);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getAddress().contains(key)
-                    || person.getPhone().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -27,4 +27,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> expected = new ArrayList<>();
         assertThat(expected, is(persons));
     }
+
+    @Test
+    public void whenFindByNameHLF() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
 }
