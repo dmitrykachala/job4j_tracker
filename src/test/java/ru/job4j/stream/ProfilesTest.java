539a31787d8d8f032e01e0fbe5db8b3f7961e0e8
dmitrykachala <kosas@mail.ru>
2021-09-22 16:15:41 +0300

    2. Список адресов [#110062]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+import ru.job4j.lambda.FunctionalCalculator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.function.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void collect() {
+        Address adr1 = new Address("Moscow", "qwert", 1, 3);
+        Address adr2 = new Address("Mo", "qw", 10, 30);
+        Address adr3 = new Address("Mos", "qwe", 11, 31);
+        Address adr4 = new Address("Mosc", "qwer", 12, 32);
+        Profile prof1 = new Profile();
+        Profile prof2 = new Profile();
+        Profile prof3 = new Profile();
+        Profile prof4 = new Profile();
+        prof1.setAddress(adr1);
+        prof2.setAddress(adr2);
+        prof3.setAddress(adr3);
+        prof4.setAddress(adr4);
+        List<Profile> prof = List.of(prof1, prof2, prof3, prof4);
+        Profiles pr = new Profiles();
+        List<Address> result = pr.collect(prof);
+        List<Address> expected = List.of(adr1, adr2, adr3, adr4);
+        assertThat(result, is(expected));
+    }
+}
